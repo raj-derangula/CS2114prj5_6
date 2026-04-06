564756c85a2de8a82de06324b79b16015ade184b
Arhan Sethi <arhansethi@vt.edu>
2026-04-06 17:30:58 -0400

    Update ahnafh6020.java

--- src/prj5/ahnafh6020.java
+++ src/prj5/ahnafh6020.java
@@ -1,8 +1,14 @@
 package prj5;
-@SuppressWarnings("javadoc")
 public class ahnafh6020
 {
+    // ----------------------------------------------------------
+    /**
+     * arhan
+     * @param args
+     */
+    public static void main(String[] args) {
 
+        System.out.println("Glad to be on your team");
 
-
+    }
 }
