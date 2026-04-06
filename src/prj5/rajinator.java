d04720431444699ee56aa6b5e7481483c3ea2560
ahnafh6020 <ahnafh6020@gmail.com>
2026-04-06 17:31:42 -0400

    Update rajinator.java

--- src/prj5/rajinator.java
+++ src/prj5/rajinator.java
@@ -3,5 +3,10 @@ package prj5;
 @SuppressWarnings("javadoc")
 public class rajinator
 {
+    public static void main(String[] args) {
+
+        System.out.println("Glad to be on your team");
+
+    }
 
 }
